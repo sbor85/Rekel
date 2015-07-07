14694e8fb6017b0fa9d6b98611968d60a211842f
Chris Kong <christopher.j.kong@gmail.com>
2015-07-07 21:46:47 +0800

    Fixed FBSDK Issues. Added FB Login Button.

--- Rekel/Rekel/LogInViewController.swift
+++ Rekel/Rekel/LogInViewController.swift
@@ -7,16 +7,44 @@
 //
 
 import UIKit
+import FBSDKLoginKit
 
 class LogInViewController: UIViewController
 {
+  var dictionary: NSDictionary!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    var testObject = PFObject(className: "TestObject")
-    testObject["foo"] = "test"
-    testObject.saveInBackground()
   }
   
+  @IBAction func fbLoginButtonClicked(sender: UIButton)
+  {
+    var fbLoginManager: FBSDKLoginManager = FBSDKLoginManager()
+    // var permissions = ["public_profile", "email", "user_friends"]
+    fbLoginManager.logInWithReadPermissions(["email"], handler: { (result, error) -> Void in
+      if (error == nil) {
+        var fbLoginResult: FBSDKLoginManagerLoginResult = result
+        if (fbLoginResult.grantedPermissions.contains("email"))
+        {
+          self.getFBUserData()
+          fbLoginManager.logOut()
+        }
+      }
+    })
+  }
+  
+  func getFBUserData() {
+    if((FBSDKAccessToken.currentAccessToken()) != nil) {
+      FBSDKGraphRequest(graphPath: "me", parameters: ["fields": "id, name, first_name, last_name, picture.type(large), email"]).startWithCompletionHandler({ (connection, result, error) -> Void in
+        if (error == nil) {
+          self.dictionary = result as! NSDictionary
+          println(result)
+          println(self.dictionary)
+          NSLog(self.dictionary.objectForKey("picture")?.objectForKey("data")?.objectForKey("url") as! String)
+        }
+      })
+    }
+  }
 
 }
